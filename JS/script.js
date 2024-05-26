0597af37ea0029acf8d1fffd7dc62214ef5a609b
Adrian <140347792+AdrianDominick@users.noreply.github.com>
2024-05-26 18:23:47 -0300

    Meu curriculo

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const discordLink = document.getElementById('discord-link');
+    discordLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      alert("Meu Discord: @furiouz ou @adriandominick");
+    });
+  });
+  
